ccd5cc0f71b3f1910c4afb45f4a94cdae584cbd6
Tianyi-Li <tianyityl@gmail.com>
2020-11-20 23:39:56 -0500

    put github config to applicationproperties

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Forum App
 
 ## Spring Boot
-## Spring MVC and Thymeleaf
+## [Spring MVC and Thymeleaf](https://spring.io/guides/gs/serving-web-content/) 
+## [Bootstrap](https://getbootstrap.com/docs/4.5/getting-started/introduction/)
---
+++ src/main/java/com/forum/community/provider/GithubProvider.java
@@ -0,0 +1,54 @@
+package com.forum.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.forum.community.dto.AccessTokenDTO;
+import com.forum.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import javax.swing.plaf.IconUIResource;
+import java.io.IOException;
+
+// initialize this to spring context
+// Don't need to new GithubProvider, it instaniates an object in a pool
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return  token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization","token " + accessToken)
+//                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+
+
+    }
+}
